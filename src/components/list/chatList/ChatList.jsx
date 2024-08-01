107f4e89ba5a095d5c3428a7e9f1a46772a49d61
Mauro Lavie <maurolavie@gmail.com>
2024-08-01 14:21:35 -0300

    Add changes to chatlist

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react"
 import "./chatList.css"
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div className="chatList">ChatList</div>
+    <div className="chatList">
+        <div className="search">
+            <div className="searchBar">
+                <img src="./search.png" alt="" />
+                <input type="text" placeholder="Search" />
+            </div>
+            <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add"
+            onClick={() => setAddMode(prev => !prev)}/>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" className="avatarImg"/>
+            <div className="texts">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" className="avatarImg"/>
+            <div className="texts">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" className="avatarImg"/>
+            <div className="texts">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" className="avatarImg"/>
+            <div className="texts">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" className="avatarImg"/>
+            <div className="texts">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
