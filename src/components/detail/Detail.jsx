700cee7bba86b71021c3427ee832087d1d93d5e4
Mauro Lavie <maurolavie@gmail.com>
2024-08-03 12:22:30 -0300

    Right menu components and css changes

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,71 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className="detail">Detail</div>
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" className="rightUpAvatar" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" className="imgArrow"/>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" className="imgArrow"/>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" className="imgArrow"/>
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img className="imgPhotosItemDetail" src="https://media.ambito.com/p/a6e46d3f69d2b96f93776f7e22408140/adjuntos/239/imagenes/040/363/0040363715/1200x675/smart/messi-camiseta-argentinajpg.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img className="imgPhotosItemDetail" src="https://media.ambito.com/p/a6e46d3f69d2b96f93776f7e22408140/adjuntos/239/imagenes/040/363/0040363715/1200x675/smart/messi-camiseta-argentinajpg.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img className="imgPhotosItemDetail" src="https://media.ambito.com/p/a6e46d3f69d2b96f93776f7e22408140/adjuntos/239/imagenes/040/363/0040363715/1200x675/smart/messi-camiseta-argentinajpg.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img className="imgPhotosItemDetail" src="https://media.ambito.com/p/a6e46d3f69d2b96f93776f7e22408140/adjuntos/239/imagenes/040/363/0040363715/1200x675/smart/messi-camiseta-argentinajpg.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"/>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" className="imgArrow"/>
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
   )
 }
 
