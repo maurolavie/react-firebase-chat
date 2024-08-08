7ce31ba9aa45eabe8171ff759d6ca6d9305e36f1
Mauro Lavie <maurolavie@gmail.com>
2024-08-08 20:23:08 -0300

    Login, logout all with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import { useEffect } from "react"
 import Chat from "./components/chat/Chat"
 import Detail from "./components/detail/Detail"
 import List from "./components/list/List"
 import Login from "./components/login/Login"
 import Notification from "./components/notification/Notification"
+import { onAuthStateChanged } from "firebase/auth"
+import { useUserStore } from "./lib/userStore"
+import { auth } from "./lib/firebase"
 
 
 const App = () => {
 
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if ( isLoading ) return <div className="loading">Loading...</div>;
 
   return (
     <div className='container'>
       {
-        user ? (
+        currentUser ? (
           <>
             <List/>
             <Chat/>
--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,11 +1,15 @@
 import "./userInfo.css"
+import { useUserStore } from "../../../lib/userStore";
 
 const Userinfo = () => {
+
+  const { currentUser } = useUserStore();
+
   return (
     <div className="userInfo">
         <div className="user">
-            <img src="./avatar.png" alt="" className="avatarImg" />
-            <h2>John Doe</h2>
+            <img src={currentUser.avatar || "./avatar.png"} alt="" className="avatarImg" />
+            <h2>{currentUser.username}</h2>
         </div>
         <div className="icons">
             <img src="./more.png" alt="" />
