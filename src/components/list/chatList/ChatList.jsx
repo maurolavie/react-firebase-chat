c4d9468115ac4e1159058d4d40dee562ec499c38
Mauro Lavie <maurolavie@gmail.com>
2024-08-30 12:41:21 -0300

    Add Search user. All broken don't work

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -10,6 +10,7 @@ const ChatList = () => {
 
   const [chats, setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
+  const [input, setInput] = useState("");
 
   const { currentUser } = useUserStore();
   const { chatId, changeChat } = useChatStore();
@@ -60,17 +61,19 @@ const ChatList = () => {
     }
   };
 
+  const filteredChats = chats.filter(c => c.username.toLowerCase().includes(input.toLowerCase()));
+
   return (
     <div className="chatList">
         <div className="search">
             <div className="searchBar">
                 <img src="./search.png" alt="" />
-                <input type="text" placeholder="Search" />
+                <input type="text" placeholder="Search" onChange={(e) => setInput(e.target.value)}/>
             </div>
             <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add"
             onClick={() => setAddMode(prev => !prev)}/>
         </div>
-        {chats.map((chat) => (
+        {filteredChats.map((chat) => (
         <div 
           className="item" 
           key={chat.chatId} 
@@ -79,16 +82,27 @@ const ChatList = () => {
             backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
           }}
         >
-            <img src={chat.user.avatar || "./avatar.png"} alt="" className="avatarImg"/>
+            <img 
+              src={
+                chat.user.blocked.includes(currentUser.id)
+                 ? "./avatar.png" 
+                 : chat.user.avatar || "./avatar.png"
+                }
+                alt="" className="avatarImg"
+            />
             <div className="texts">
-                <span>{chat.user.username}</span>
+                <span>
+                  {chat.user.blocked.includes(currentUser.id)
+                   ? "User" 
+                   :  chat.user.username}
+                </span>
                 <p>{chat.lastMessage}</p>
             </div>
         </div>
         ))}
         {addMode && <AddUser/>}
     </div>
-  )
+ ? "User" : currentUser.id)
 }
 
 export default ChatList
